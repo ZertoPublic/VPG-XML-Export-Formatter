6b977b8689af220fa802656ee2edc49b0c4aed09
LouS <55985453+lousZerto@users.noreply.github.com>
2019-10-01 22:54:05 +1000

    Add files via upload

--- parse-xml.py
+++ parse-xml.py
@@ -112,6 +112,12 @@ def ProcessXMLfileCSVRaw():
                         cc=str(C3)
                         s0,s1 = cc.split("}")
                         s0,S1 = s1.split("'")
+                        #
+                        ## - Check if we've found the VPG name if so replcae the "Name" VPG field with a
+                        ## - "VPG-Name" field to make identifictaion of the VPG name easier
+                        #
+                        if s0 == "Name":
+                            s0 = "VPG-Name"
                         print (s0,",",C3.text,",", end="")
                         #
                         ## - Third Child Element
@@ -238,6 +244,10 @@ def ProcessXMLfile():
                         VPGdata=("VPGN"+str(VPGcount),str(s0),str(C3.text))
                         VWF.write(str(VPGdata))
                         VWF.write("\n")
+                        #
+                        ## - Check if we've found the VPG name if so add a new VPG Field into the temp file
+                        ## - which will be used to identify the VPG number and label them in the CSV file
+                        #
                         if s0 == "Name":
                             VPGdata = ("VPGN"+str(VPGcount),"VPG-Name=", str(C3.text))
                             VWF.write(str(VPGdata))
